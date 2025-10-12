da252f02fec2b5aced40ec2bdc71c5f57094067d
dattv23 <datvan635@gmail.com>
2025-10-12 10:54:16 +0700

    refactor: display scores in mobile

--- src/components/GameScores.tsx
+++ src/components/GameScores.tsx
@@ -10,15 +10,15 @@ const GameScores: React.FC<GameScoresProps> = ({ scores }) => {
   return (
     <div className='flex justify-between'>
       <div className='flex w-24 flex-col items-center rounded-xl bg-[#31C3BD] px-5 py-2 shadow-[inset_0_-4px_0_#1E8E89] md:w-32'>
-        <p className='font-bold'>X (YOU)</p>
+        <p className='text-xs font-semibold'>X (YOU)</p>
         <span className='text-xl leading-4 font-semibold'>{scores['X']}</span>
       </div>
       <div className='flex w-24 flex-col items-center rounded-xl bg-[#a8bfc9] px-5 py-1 shadow-[inset_0_-4px_0_#6b8997] md:w-32'>
-        <p className='font-bold'>Draw</p>
+        <p className='text-xs font-semibold'>Draw</p>
         <span className='text-xl leading-4 font-semibold'>{scores['Draw']}</span>
       </div>
       <div className='flex w-24 flex-col items-center rounded-xl bg-[#F2B137] px-5 py-1 shadow-[inset_0_-4px_0_#C48524] md:w-32'>
-        <p className='font-bold'>O (AI)</p>
+        <p className='text-xs font-semibold'>O (AI)</p>
         <span className='text-xl leading-4 font-semibold'>{scores['O']}</span>
       </div>
     </div>
